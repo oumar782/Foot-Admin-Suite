cf91ce9b2c07d500d0fe701db954a8010061aa40
Oumar Diane <oumardiane399@gmail.com>
2025-05-19 16:59:26 +0100

    first commit21

--- src/page/Authentification.jsx
+++ src/page/Authentification.jsx
@@ -48,8 +48,7 @@ const AuthComponent = () => {
     setIsLoading(true);
     
     try {
-      const response = await fetch('https://api-footadmin.vercel.app/api/auth/login', ...)
-      , {
+      const response = await fetch('https://foot-admin-suite.vercel.app/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nom, email, motdepasse, role })
